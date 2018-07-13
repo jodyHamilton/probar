170d830f0d8b2f7a19265287e4180fdbc42ec84c
Jody Hamilton <jody@zivtech.com>
2018-07-13 17:38:00 -0400

    Update probar.js

--- probar.js
+++ probar.js
@@ -1,17 +1,19 @@
 // Include CSS.
 document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"https://rawgit.com/jodyHamilton/probar/master/probar.css\" />");
 
-// Close button.
-var probarClose = document.createElement("div");
-probarClose.setAttribute("id", "probar-close");
-probarClose.addEventListener('click', function(e) {
-  probar.setAttribute("id", "probar-closed");
-  probar.innerHTML('');
-});
-
 // Add the Probar.
 var probar = document.createElement("div");
 probar.setAttribute("id", "probar-closed");
+
+// Close button.
+var probarClose = document.createElement("div");
+probarClose.setAttribute("id", "probar-close");
+probarClose.innerHTML = '&#10006;';
+probarClose.addEventListener('click', function(e) {
+  // TO FIX
+  document.getElementById("probar-open").innerHTML = '';
+  document.getElementById("probar-open").setAttribute("id", "probar-closed");
+});
 
 probar.addEventListener('click', function(e) {
   if (this.getAttribute("id") == "probar-closed") {
