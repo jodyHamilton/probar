acc705628d3361c1cc3e46d02e6a1c5103701f5d
Jody Hamilton <jody@zivtech.com>
2018-07-13 16:23:13 -0400

    Update probar.js

--- probar.js
+++ probar.js
@@ -1,12 +1,7 @@
+// Include CSS.
+document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"https://rawgit.com/jodyHamilton/probar/master/probar.css\" />");
 
-var link  = document.createElement('link');
-link.rel  = 'stylesheet';
-link.type = 'text/css';
-link.href = 'https://raw.githubusercontent.com/jodyHamilton/probar/master/probar.css';
-link.media = 'all';
-document.getElementsByTagName('head')[0].appendChild(link);
-
-
+// Add the Probar.
 var probar = document.createElement("div");
 probar.setAttribute("id", "probar");
 document.getElementsByTagName('body')[0].appendChild(probar);
