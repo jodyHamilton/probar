52ccc6795ab97840a4877b156eca032f9bf3b618
Jody Hamilton <jody@zivtech.com>
2018-07-13 17:12:21 -0400

    Update probar.js

--- probar.js
+++ probar.js
@@ -4,7 +4,15 @@ document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link 
 // Add the Probar.
 var probar = document.createElement("div");
 probar.setAttribute("id", "probar-closed");
+
+var probarClose = document.createElement("div");
+probarClose.setAttribute("id", "probar-close");
+
 probar.addEventListener('click', function(e) {
-  this.setAttribute("id", "probar-open");
+  if (this.getAttribute("id") == "probar-closed") {
+    this.setAttribute("id", "probar-open");
+    this.appendChild(probarClose);
+    console.log('click');
+  }
 });
 document.getElementsByTagName('body')[0].appendChild(probar);
