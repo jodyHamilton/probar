4095e8dd13b61f245f464344127eb04a8a7351a2
Jody Hamilton <jody@zivtech.com>
2018-07-13 16:48:52 -0400

    Update probar.js

--- probar.js
+++ probar.js
@@ -3,5 +3,8 @@ document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link 
 
 // Add the Probar.
 var probar = document.createElement("div");
-probar.setAttribute("id", "probar");
+probar.setAttribute("id", "probar-closed");
+probar.addEventListener('click', function(e) {
+  this.setAttribute("id", "probar-open");
+});
 document.getElementsByTagName('body')[0].appendChild(probar);
