42f7c719ac8b1de6240f71cd302ea5e12b4c06b2
Jody Hamilton <jody@zivtech.com>
2018-07-13 16:07:43 -0400

    Update probar.js

--- probar.js
+++ probar.js
@@ -2,7 +2,7 @@
 var link  = document.createElement('link');
 link.rel  = 'stylesheet';
 link.type = 'text/css';
-link.href = '/Users/jodyhamilton/Sites/probar/css/probar.css';
+link.href = 'https://raw.githubusercontent.com/jodyHamilton/probar/master/probar.css';
 link.media = 'all';
 document.getElementsByTagName('head')[0].appendChild(link);
 
